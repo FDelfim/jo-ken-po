927920b2c3dbdc8ab852b67338aa303583c630b3
FDelfim <lipedelfim@gmail.com>
2023-01-14 19:09:26 -0300

    update main

--- src/components/game/index.tsx
+++ src/components/game/index.tsx
@@ -19,9 +19,8 @@ export default function Game() {
                 <FontAwesomeIcon icon={faHand} size='5x' className='col-3'/>
                 <FontAwesomeIcon icon={faHandPeace} size='5x' className='col-3'/>
             </PCSide>
-            <Resultado className='row mx-5 p-2 justify-content-center mt-4'>
+            <Resultado className='row mx-5 p-2 justify-content-center align-items-center mt-4'>
                 <h2 className='col-lg-12 pt-3 text-center'>Choose your option</h2>
-                TAMANHO 4
             </Resultado>
             <Options className='row mx-5 p-2 justify-content-center align-items-center mt-4'>
                 <LabelPlayer>YOU</LabelPlayer>
